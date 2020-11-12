96fb81cf4a424bd6271f7db59b442d4aadbee54c
Vikram Pal <vikramsonupal@gmail.com>
2020-11-11 16:27:28 -0800

    Setting up

---
+++ client/src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReviewsApp from './components/ReviewsApp.jsx';
+
+ReactDOM.render(<ReviewsApp />, document.getElementById('root'));
---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+const publicDirectory = path.join(__dirname, '../client/dist');
+
+app.use(express.json());
+app.use(express.static(publicDirectory));
+
+app.listen(port, () => {
+  console.log(`Express is listening on port ${port}`);
+});
