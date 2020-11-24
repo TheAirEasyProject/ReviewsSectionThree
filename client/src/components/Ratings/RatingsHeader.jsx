7fb28643b372e8a9713c41246ff6de208c55da95
Vikram Pal <vikramsonupal@gmail.com>
2020-11-24 13:05:35 -0800

    Bug Fixes

--- client/src/components/Ratings/RatingsHeader.jsx
+++ client/src/components/Ratings/RatingsHeader.jsx
@@ -3,7 +3,7 @@ import styles from '../../../../styles.css';
 
 const RatingsHeader = ({ data }) => {
   return (
-    <div className={styles.header}>
+    <div className={styles.revheader}>
       <div>
         <i className='fa fa-star' id={styles.staricon}></i>{' '}
         {data.length > 0 && data[0].listing_rating} (
