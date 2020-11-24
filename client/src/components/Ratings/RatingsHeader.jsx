a3a5f946505c02ed7abdaa8e723ea7f044c8fcae
Vikram Pal <vikramsonupal@gmail.com>
2020-11-24 12:22:58 -0800

    Style Adjustments

--- client/src/components/Ratings/RatingsHeader.jsx
+++ client/src/components/Ratings/RatingsHeader.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import styles from "../../../../styles.css";
+import React from 'react';
+import styles from '../../../../styles.css';
 
 const RatingsHeader = ({ data }) => {
   return (
     <div className={styles.header}>
-      <h3>
-        <i className="fa fa-star" id={styles.staricon}></i>{" "}
+      <div>
+        <i className='fa fa-star' id={styles.staricon}></i>{' '}
         {data.length > 0 && data[0].listing_rating} (
         {data.length > 0 && data[0].listing_reviews.length} Reviews)
-      </h3>
+      </div>
     </div>
   );
 };
