ca26b9ddd3f009a37e84f145a68fd60bfd8319e4
Vikram Pal <vikramsonupal@gmail.com>
2020-11-24 16:02:57 -0800

    Production Mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   resolve: { extensions: ['.js', '.jsx', '.json', '.css'] },
   entry: path.resolve(__dirname, 'client/src/index.jsx'),
   output: {
     path: path.resolve(__dirname, 'client/dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   module: {
     rules: [
@@ -14,17 +14,22 @@ module.exports = {
         test: /\.(jsx|js)$/,
         include: path.resolve(__dirname, 'client/src'),
         exclude: /node_modules/,
-        use: [{
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              ['@babel/preset-env', {
-                "targets": "defaults"
-              }],
-              '@babel/preset-react'
-            ]
-          }
-        }]
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    targets: 'defaults',
+                  },
+                ],
+                '@babel/preset-react',
+              ],
+            },
+          },
+        ],
       },
       {
         test: /\.(png|jpe?g|gif)$/i,
@@ -37,7 +42,8 @@ module.exports = {
       {
         test: /\.css$/i,
         exclude: /node_modules/,
-        use: ['style-loader',
+        use: [
+          'style-loader',
           {
             loader: 'css-loader',
             options: {
@@ -45,9 +51,10 @@ module.exports = {
                 localIdentName: '[name]__[local]___[hash:base64:5]',
               },
             },
-          }],
+          },
+        ],
       },
-    ]
+    ],
   },
   watch: true,
-}
+};
